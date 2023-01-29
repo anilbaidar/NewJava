25913cddd32d17156f394c98952777aa4a21618d
anil baidar <anil.baidar23@gmail.com>
2023-01-29 15:56:21 -0600

    3rd commit

--- src/main/java/GitPractice.java
+++ src/main/java/GitPractice.java
@@ -2,5 +2,6 @@ public class GitPractice {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("new line");
+        System.out.println("third line");
     }
 }
